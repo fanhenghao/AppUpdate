7226bfe4f9e892799911675fb12f043f9cab80fc
fanhenghao <95024708@qq.com>
2019-08-21 15:02:43 +0800

    优化调整版本更新功能

--- app/src/main/java/com/android/app/appupdate/DownloadService.java
+++ app/src/main/java/com/android/app/appupdate/DownloadService.java
@@ -55,11 +55,10 @@ public class DownloadService extends Service {
         DownloadManager dManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
         Uri uri = Uri.parse(url);
         DownloadManager.Request request = new DownloadManager.Request(uri);
-
+        //这里的"AppUpdate.apk"要对应DOWNLOAD_PATH的"AppUpdate.apk"
         request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "AppUpdate.apk");
         request.setDescription("新版本下载中...");
         request.setTitle("版本更新");
-
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
         request.setMimeType("application/vnd.android.package-archive");
         // 设置为可被媒体扫描器找到
@@ -85,8 +84,9 @@ public class DownloadService extends Service {
                     install.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     install.addCategory(Intent.CATEGORY_DEFAULT);
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {//大于Android版本7.0
+                        //这里的"com.android.app.appupdate.provider"需要和AndroidManifest.xml的provider节点下的authorities属性保持一致
                         Uri contentUri = FileProvider.getUriForFile(context, "com.android.app.appupdate.provider", new File(DOWNLOAD_PATH));
-                        install.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                        install.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);//这里不能是setFlags(),set会覆盖掉之前的flags
                         install.setDataAndType(contentUri, "application/vnd.android.package-archive");
                     } else {
                         try {
@@ -121,6 +121,4 @@ public class DownloadService extends Service {
     public void onDestroy() {
         unregisterReceiver(receiver);
     }
-
-
 }
